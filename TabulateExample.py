8d0f5a7d2648697181865ae830dfb0121287c8ea
Dither Dude <88754523+DitherDude@users.noreply.github.com>
2024-09-08 16:26:18 +1000

    Update TabulateExample.py
    Updated comments,
    Removed unnecessary Regex import.

--- TabulateExample.py
+++ TabulateExample.py
@@ -1,15 +1,14 @@
-# tabulate is accessibe now through 'tb' instead of
+# Writing the import line as such makes
+# Tabulate accessibe through 'tb' instead of
 # 'tabulate.tabulate' for easier use.
-import re
 from tabulate import tabulate as tb
-
 print("(1) On your marks...")
-# end="" tells python not to go down to the next line,
-# so the next print() command continues on that line.
+# end="" tells python not to go down to the next line of the
+# console output, so the next print() command continues on that line.
 print("(2) Locating data... ", end="")
 # Try to open the data file.
 try:
-    # Data file is written in the following format:
+    # The proovided sample data file is written in the following format:
     # Name,YearOfBirth,Age,Carrots
     # e.g. Billy,2000,24,5
     file = open("RabbitBytes.csv", "r")
@@ -21,15 +20,18 @@ except FileNotFoundError:
 print("done!")
 print("(3) Loading data...", end="")
 rawdata = file.readlines()
-# remove the \n character from the end of each line
+# Remove the \n character from the end of each line
 for i in range(len(rawdata)):
     rawdata[i] = rawdata[i].replace("\n", "")
-# now we need to convert the data so tabulate understands it -
+# Now we need to convert the data so tabulate understands it -
 # from [..., ..., ..., ...] to [[...],[...],[...],[...]]
+# as it is currently just a string array.
+# However, tabulate needs an array of arrays to mimic
+# a table format.
 data = []
 for bit in rawdata:
     bit = bit.split(",")
     data.append(bit)
 print("done!\n")
-# looks good! now we can print it.
+# Looks good! now we can print it.
 print(tb(data, headers=["Name", "YoB", "Age", "Carrots"]))
